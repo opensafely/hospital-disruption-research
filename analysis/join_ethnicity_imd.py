7372d6d05cd1a917b64de946fd37f689c01ceb63
LFISHER7 <lhf282@live.co.uk>
2021-02-26 17:37:36 +0000

    add exclusion for imd

--- analysis/join_ethnicity_imd.py
+++ analysis/join_ethnicity_imd.py
@@ -9,7 +9,7 @@ imd_df = pd.read_csv('output/input_imd.csv')
 for file in os.listdir('output'):
     if file.startswith('input'):
         #exclude ethnicity
-        if file.split('_')[1] != 'ethnicity.csv':
+        if file.split('_')[1] not in ['ethnicity.csv', 'imd.csv']:
             file_path = os.path.join('output', file)
             df = pd.read_csv(file_path)
             merged_df = df.merge(ethnicity_df, how='inner', on='patient_id')
