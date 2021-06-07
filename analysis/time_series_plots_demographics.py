909f9029eca2fe143942ede844811e19dff51f56
LFISHER7 <lhf282@live.co.uk>
2021-06-07 01:30:41 +0100

    correct plot ylims

--- analysis/time_series_plots_demographics.py
+++ analysis/time_series_plots_demographics.py
@@ -8,6 +8,8 @@ populations = ['total', 'emergency', 'elective']
 demographic_variables = ["ethnicity", "imd_group", "sex"]
 ethnicity_codes = {'1.0': "White", '2.0': "Mixed", '3.0': "Asian", '4.0': "Black", '5.0':"Other", np.nan: "unknown"}
 
+y_lims = {'total': 180, 'emergency': 90 , 'elective': 110}
+
 for pop in populations:
     df_dict = combined_diseases[pop]
     
@@ -41,8 +43,7 @@ for pop in populations:
 
             axes[i].legend(loc=3, prop={"size": 9})
             axes[i].set_ylabel("Rate per 100,000 people")
-            axes[i].set_ylim(ymin=0)
-#             axes[i].set_ylim(ymin=0, ymax=figure_cut_offs[disease])
+            axes[i].set_ylim(ymin=0, ymax=y_lims[pop])
             
           
             plt.tight_layout()
